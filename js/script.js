5faec27812652c8f194524e42330f998cbdeffbf
Some0neElse <cyber-def@yandex.ru>
2020-04-13 21:41:44 +0300

    fixes for 2nd iteration

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
 const editButton = document.querySelector(".profile__edit-button");
 const popup = document.querySelector(".popup");
 const closeButton = document.querySelector(".popup__close-button");
-const formElement = document.querySelector(".popup__container")
+const formElement = document.querySelector(".popup__container");
+const profileFullName = document.querySelector(".profile__full-name");
+const profileVocation = document.querySelector(".profile__vocation");
+const popupFullName = document.querySelector("#fullname");
+const popupVocation = document.querySelector("#vocation");
+
+
+console.log(profileFullName.value);
+console.log(profileFullName.textContent);
 
 function editButtonClick() {
-  document.querySelector(".popup__full-name").value = document.querySelector(".profile__full-name").textContent;
-  document.querySelector(".popup__vocation").value = document.querySelector(".profile__vocation").textContent;
-  popup.classList.toggle("popup_closed");
-  popup.classList.toggle("popup_opened");
+  popupFullName.value = profileFullName.textContent;
+  popupVocation.value = profileVocation.textContent;
+  popup.classList.remove("popup_closed");
+  popup.classList.add("popup_opened");
 }
 
 function closeButtonClick() {
-  popup.classList.toggle("popup_closed");
-  popup.classList.toggle("popup_opened");
+  popup.classList.add("popup_closed");
+  popup.classList.remove("popup_opened");
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-    document.querySelector(".profile__full-name").textContent = document.querySelector(".popup__full-name").value;
-    document.querySelector(".profile__vocation").textContent =  document.querySelector(".popup__vocation").value;
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileFullName.textContent = popupFullName.value;
+  profileVocation.textContent = popupVocation.value;
+  popup.classList.add("popup_closed");
+  popup.classList.remove("popup_opened");
 }
+
+
 editButton.addEventListener("click", editButtonClick);
 closeButton.addEventListener("click", closeButtonClick);
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
 
-/*В принципе, можно всю разметку формы вставлять в html в момент вызова формы, но нужно ли? */
+
