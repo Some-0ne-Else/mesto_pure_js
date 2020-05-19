b07172e81b70dac64d5b1cdb24c362660f1f2bd3
Some0neElse <cyber-def@yandex.ru>
2020-05-19 23:54:36 +0300

    almost ready for 1st iteration

--- js/script.js
+++ js/script.js
@@ -25,6 +25,10 @@ const initialElements = [
   }
 ];
 
+/* Пришлось отказаться от переиспользования одного и того же шаблона popup (был единый для форм редактирования и добавления т.к. идентичны по компоновке на 100%) ввиду задания 6 работы
+ прямо указывающего на использование стандартных сообщений об ошибках при валидации форм. */
+
+
 const editButton = document.querySelector(".profile__edit-button");
 const addButton = document.querySelector(".profile__add-button");
 const elements = document.querySelector('.elements'); //цель для вставки новых элементов на страницу
@@ -44,8 +48,8 @@ const closeButton = document.querySelector(".popup__close-button");
 const popupActionButton = document.querySelector(".popup__action-button");
 const formElement = document.querySelector(".popup__container");
 const popupHeading = document.querySelector(".popup__heading");
-const popupFirstInput = document.querySelector("#first-input");
-const popupSecondInput = document.querySelector("#second-input");
+const popupName = document.querySelector("#name");
+const popupUrl = document.querySelector("#url");
 
 const popupEnlarged = document.querySelector(".popup-enlarged");
 const popupEnlargedImage = document.querySelector(".popup-enlarged__image");
@@ -69,6 +73,7 @@ function deleteElementButtonHandler(evt) {
 function elementImageHandler(evt) {
   popupEnlargedImage.src = evt.target.src;
   popupEnlargedCaption.textContent = evt.target.alt;
+  document.addEventListener('keyup', closeAnyPopupAtEscape);
   popupEnlarged.classList.toggle("popup-enlarged_opened");
 };
 
@@ -87,6 +92,16 @@ function makeListenerDeleteButton() {
   deleteButton.addEventListener('click', deleteElementButtonHandler);
 };
 
+
+//функция закрытия любого попапа по Esc
+function closeAnyPopupAtEscape(evt){
+  if(evt.key === 'Escape'){
+  popupEnlargedCloseButtonHandler();
+  closeButtonHandler();
+  popupEditProfileCloseButtonHandler();
+}
+};
+
 // функция рендеринга одного элемента
 function renderElement(item) {
   const elementTemplate = document.querySelector('.element__template').content;
@@ -99,10 +114,8 @@ function renderElement(item) {
 }
 //функция добавления элемента на страницу принимает пар1 элемент пар2 куда вставляем
 function appendElement(element, targetElement) {
-  // отображаем на странице
-  //const elements = document.querySelector('.elements'); //цель для вставки
-  targetElement.prepend(renderElement(element));
-  //создаем ждунов слушателей
+  targetElement.prepend(renderElement(element));  // отображаем на странице
+  //создаем ждунов-слушателей
   makeListenerLikeButton(document.querySelector(".element__like"));
   makeListenerDeleteButton();
   makeListenerElementImage();
@@ -118,58 +131,65 @@ appendInitialElements();
 
 // функция очистки значений popup раз уж мы пошли путем эксплуатации одной html формы -)
 function cleanPopupValues() {
-  popupFirstInput.value = null;
-  popupSecondInput.value = null;
+  popupName.value = null;
+  popupUrl.value = null;
 }
 //функция удаления обработчиков
 function removeFormEventListeners() {
   formElement.removeEventListener("submit", formEditHandler);
   formElement.removeEventListener("submit", formAddHandler);
+  document.addEventListener('click',closeAnyPopupAtEscape);
 }
 
 //функция обработки нажатия на кнопку редактировать
 function editButtonHandler() {
-  // очищать не нужно т.к. данные попапа затираются данными со страницы
   popupEditProfileFullName.value = profileFullName.textContent;
   popupEditProfileVocation.value = profileVocation.textContent;
+  const popupEditProfileInactiveButtonClass = 'popup-edit-profile__action-button_disabled';
+  const popupEditProfileAnyInput = ".popup-edit-profile__input";
+  const popupEditProfileActionButtonClass= ".popup-edit-profile__action-button";
+  refreshButtonState(popupEditProfileFormElement, popupEditProfileAnyInput,popupEditProfileActionButtonClass, popupEditProfileInactiveButtonClass); //обновим состояние кнопки
   popupEditProfileFormElement.addEventListener("submit", formEditHandler);
+  //document.addEventListener('keyup', (evt => { if(evt.key === 'Escape'){popupEditProfileCloseButtonHandler()} }));
+  document.addEventListener('keyup', closeAnyPopupAtEscape);
   popupEditProfile.classList.add("popup-edit-profile_opened");
 }
+
 
 //функция обработки нажатия на кнопку закрыть
 function closeButtonHandler() {
   popup.classList.remove("popup_opened");
-  removeFormEventListeners(); // проверить функцию
+  removeFormEventListeners();
 }
 
-function popupEditProfileCloseButtonHandler() {
+function popupEditProfileCloseButtonHandler(evt,somevar) {
   popupEditProfile.classList.remove("popup-edit-profile_opened");
-  removeFormEventListeners(); // проверить функцию
+  removeFormEventListeners();
 }
-
 
 //функция обработки нажатия на кнопку добавить
 function addButtonHandler() {
   cleanPopupValues();
   popupHeading.textContent = "Новое место";
-  popupFirstInput.placeholder = "Название";
-  popupSecondInput.placeholder = "Ссылка на картинку";
+  popupName.placeholder = "Название";
+  popupUrl.placeholder = "Ссылка на картинку";
   popupActionButton.textContent = "Создать";
   formElement.addEventListener("submit", formAddHandler);
+  document.addEventListener('keyup', closeAnyPopupAtEscape);
   popup.classList.add("popup_opened");
 }
 //функция обработчик формы редактирования
 function formEditHandler(evt) {
   evt.preventDefault();
-  profileFullName.textContent = popupFirstInput.value;
-  profileVocation.textContent = popupSecondInput.value;
-  popup.classList.remove("popup_opened");
+  profileFullName.textContent = popupEditProfileFullName.value;
+  profileVocation.textContent = popupEditProfileVocation.value;
+  popupEditProfile.classList.remove("popup-edit-profile_opened");
   removeFormEventListeners();
 }
 //функция обработчик формы добавления
 function formAddHandler(evt) {
   evt.preventDefault();
-  initialElements.push({ title: popupFirstInput.value, imageLink: popupSecondInput.value });
+  initialElements.push({ title: popupName.value, imageLink: popupUrl.value });
   appendElement(initialElements[initialElements.length - 1], elements);
   popup.classList.remove("popup_opened");
   removeFormEventListeners();
@@ -179,18 +199,14 @@ function popupEnlargedCloseButtonHandler() {
   popupEnlarged.classList.remove("popup-enlarged_opened");
 }
 
+
 //ждуны слушатели
 editButton.addEventListener("click", editButtonHandler);
+popupEditProfileCloseButton.addEventListener('click',popupEditProfileCloseButtonHandler);
 closeButton.addEventListener("click", closeButtonHandler);
 addButton.addEventListener("click", addButtonHandler);
 popupEnlargedCloseButton.addEventListener("click", popupEnlargedCloseButtonHandler);
 
-//testzone
-// Закрытие всплывающих окон по Esc вешать при открытии попапа и снимать при закрытии
-document.addEventListener("keyup", evt => { if(evt.keyCode === 27){console.log(evt.key);popupEnlargedCloseButtonHandler();closeButtonHandler();}});
-popup.addEventListener("click", evt => {popupEnlargedCloseButtonHandler();closeButtonHandler();} );
-// popup.addEventListener('keyup', function (evt) {
-//   if (evt.keyCode === 27){
-//     closeButtonHandler();
-//   }
-// });
+
+
+popupEditProfile.addEventListener('click',popupEditProfileCloseButtonHandler);
---
+++ js/validate.js
@@ -0,0 +1,98 @@
+//функция вывода ошибки  принимает пар1 форму, пар2 input элемент, пар3 сообщение об ошибке
+const showInputError = (formElement, inputElement, errorMessage, inputErrorClass, errorClass) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`); //объявляем константу значением которой является имя инпут элемента с постфиксом "-error"
+  inputElement.classList.add(inputErrorClass); //навешиваем инпуту модификатор  с классом ошибки
+  errorElement.textContent = errorMessage; //выводим сообщение об ошибке переданное параметром 3
+  errorElement.classList.add(errorClass); //навешиваем тексту ошибки класс модификатор для отображентия ошибки
+};
+//вспомогательная функция проверяющая не содержит ли один из элементов списка валидности, не валидный в данный момент параметр
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+    // Если хотя бы одно поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся фунцкция
+    // hasInvalidInput вернёт true
+    return !inputElement.validity.valid; //Если хотя бы одно поле не валидно, вернем true
+  })
+};
+
+// Тогглер активности кнопки пар1 список элементов проверки пар2 кнопка
+const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
+  if (hasInvalidInput(inputList)) { //если функция вернула true (значит есть ошибка в одном и более параметрах валидации)
+    buttonElement.classList.add(inactiveButtonClass); //добавляем модификатор неактивности кнопки
+  } else {
+    buttonElement.classList.remove(inactiveButtonClass); // иначе убираем модификатор
+  }
+}
+
+// функция убирающая вывод ошибки (пар1 форма, пар2 нужный нам инпут)
+const hideInputError = (formElement, inputElement, inputErrorClass, errorClass) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`); //находим нужный инпут по id и ожидаем что у него будет постфикс "-error"
+  inputElement.classList.remove(inputErrorClass); //снимаем с инпута класс ошибки
+  errorElement.classList.remove(errorClass); //снимаем поля ошибки класс показывающий текущую ошибку
+  errorElement.textContent = ''; //очищаем текст сообщения об ошибке
+};
+
+//функция проверки валидности элемента формы пар1 форма, пар2 нужный инпут
+const checkInputValidity = (formElement, inputElement, inputErrorClass, errorClass) => {
+  if (!inputElement.validity.valid) { //если у инпута состояние валидности ложно
+    showInputError(formElement, inputElement, inputElement.validationMessage, inputErrorClass, errorClass); //вызываем описанную ранее функцию отображения ошибки с тремя параметрам пар1 форма, пар2 инпут, пар3 сообщение валидации
+  } else {
+    hideInputError(formElement, inputElement, inputErrorClass, errorClass); //иначе вызываем функцию убирающую сообщение об ошибке
+  }
+};
+
+//функция навешивающая ждунов-слушателей на форму
+const setEventListeners = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector)); //соберем все инпуты  формы и сделаем из них массив
+  const buttonElement = formElement.querySelector(submitButtonSelector); //присвоим новой константе селектор кнопки отправки обрабатываемой формы
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass); // потыкаем палочкой кнопку чтобы задать ей актуальное состояние
+
+  inputList.forEach((inputElement) => { //для каждого элемента из собранного массива инпутов
+    inputElement.addEventListener('input', function () { //добавим по ждуну-слушателю
+      toggleButtonState(inputList, buttonElement, inactiveButtonClass); //и конечно потыкаем палочкой тоглер кнопки
+      checkInputValidity(formElement, inputElement, inputErrorClass, errorClass); //вызовем функцию проверки валидности элемента формы
+    });
+  });
+};
+
+//функция перепроверяющая актуальность состояния кнопки, в случае если данные полей попапа изменились позднее
+const refreshButtonState = (formElement, inputSelector, submitButtonSelector, inactiveButtonClass) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector)); //соберем все инпуты  формы и сделаем из них массив
+  const buttonElement = formElement.querySelector(submitButtonSelector);
+  toggleButtonState(inputList, buttonElement, inactiveButtonClass);
+}
+
+
+
+//убер функция включающая валидацию всех форм на странице (на самом деле только элементов с классом ".form")
+const enableValidation = ({ formSelector, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass }) => {
+  const formList = Array.from(document.querySelectorAll(formSelector)); //сделаем массив из массивоподобного списка всех форм на странице (см описание функции касательно всех)
+  formList.forEach((formElement) => { //для каждой формы
+    formElement.addEventListener('submit', function (evt) { //добавим ждуна на отправку
+      evt.preventDefault(); //предотвратим действие по умолчанию
+    });
+    setEventListeners(formElement, inputSelector, submitButtonSelector, inactiveButtonClass, inputErrorClass, errorClass);
+
+  }); //закончился 1й (внешний) форыч
+};
+
+//Для простоты и последующей интеграции с беком
+const allForms = [{
+  formSelector: '.popup-edit-profile__container',
+  inputSelector: '.popup-edit-profile__input',
+  submitButtonSelector: '.popup-edit-profile__action-button',
+  inactiveButtonClass: 'popup-edit-profile__action-button_disabled',
+  inputErrorClass: 'popup-edit-profile__input_type_error',
+  errorClass: 'popup-edit-profile__input-error_active'
+},
+{
+  formSelector: '.popup__container',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__action-button',
+  inactiveButtonClass: 'popup__action-button_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+}];
+//обработаем все формы полученные из массива
+allForms.forEach(item => enableValidation(item));
