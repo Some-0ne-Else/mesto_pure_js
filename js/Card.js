a9a67af0cf3b380bcb0023754f6af026dd0c1452
Some0neElse <cyber-def@yandex.ru>
2020-06-05 17:04:15 +0300

    almost ready for 1st iteration

---
+++ js/Card.js
@@ -0,0 +1,64 @@
+export default class Card{
+  constructor(title, url, templateSelector){
+    this._title = title;
+    this._url = url;
+    this._templateSelector = templateSelector;
+  }
+  _getTemplate() {
+    const cardElement = document
+    .querySelector(this._templateSelector)
+    .content.querySelector('.element')
+    .cloneNode(true);
+    return cardElement;
+}
+ generateCard() {
+  // Запишем разметку в приватное поле _element.
+  // Так у других элементов появится доступ к ней.
+  this._element = this._getTemplate();
+  const elementImage = this._element.querySelector('.element__image');
+  const elementTitle = this._element.querySelector('.element__title');
+
+  elementImage.src = this._url;
+  elementTitle.textContent = this._title;
+  elementImage.alt = this._title;
+
+  /*selectors for Listeners */
+  const deleteButton = this._element.querySelector('.element__delete-button');
+  const elementLike = this._element.querySelector('.element__like');
+  /* adding handlers */
+  deleteButton.addEventListener('click', this._deleteElementHandler);
+  elementLike.addEventListener('click', this._likeButtonHandler);
+  elementImage.addEventListener('click', this._elementImageHandler);
+
+  // Вернём элемент наружу
+  return this._element;
+}
+    _elementImageHandler(evt) {
+    const popupImage = document.querySelector('.popup__image');
+    popupImage.src = evt.target.src;
+    const popupCaption = document.querySelector('.popup__caption');
+    popupCaption.textContent = evt.target.alt;
+
+    const popupEnlarge = document.querySelector('.popup-enlarge');
+    document.addEventListener('keyup', (evt) => {
+      if (evt.key === 'Escape') {
+    const popupToClose = document.querySelector('.popup_opened');
+   if (popupToClose != null)  popupToClose.classList.remove('popup_opened');  }});
+    popupEnlarge.classList.add('popup_opened');
+  }
+
+  _likeButtonHandler(evt) {
+    evt.target.classList.toggle('element__like_active');
+  }
+
+  _deleteElementHandler(evt) {
+    const elementToDelete = evt.target.closest('.element');
+    const elementImage = elementToDelete.querySelector('.element__image');
+    const elementLike = elementToDelete.querySelector('.element__like');
+    elementImage.removeEventListener('click', this._elementImageHandler);
+    elementLike.removeEventListener('click', this._likeButtonHandler);
+    evt.target.removeEventListener('click', this._deleteElementHandler);
+    elementToDelete.remove();
+  }
+}
+
--- js/index.js
+++ js/index.js
@@ -1,35 +1,33 @@
-<<<<<<< HEAD
-=======
+import Card from '../js/Card.js';
+import FormValidator from '../js/FormValidator.js'
+import hideInputError from '../js/validate.js'; // используем как времнное решение до продолжения рефакторинга в след спринте (т.к. публичный метод у класса должен быть только один)
+
 const initialElements = [
   {
     title: 'Сибуя',
-    imageLink: './images/element__image_sibuya.jpg'
+    url: './images/element__image_sibuya.jpg'
   },
   {
     title: 'Прага',
-    imageLink: './images/element__image_praga.jpg'
+    url: './images/element__image_praga.jpg'
   },
   {
     title: 'Иордания',
-    imageLink: './images/element__image_iordaniya.jpg'
+    url: './images/element__image_iordaniya.jpg'
   },
   {
     title: 'Швейцария',
-    imageLink: './images/element__image_switzerland.jpg'
+    url: './images/element__image_switzerland.jpg'
   },
   {
     title: 'Греция',
-    imageLink: './images/element__image_greece.jpg'
+    url: './images/element__image_greece.jpg'
   },
   {
     title: 'Черногория',
-    imageLink: './images/element__image_montenegro.jpg'
+    url: './images/element__image_montenegro.jpg'
   }
 ];
-
-/* rendering const */
-const elements = document.querySelector('.elements'); // target for inserting new elements
-const elementTemplate = document.querySelector('.element__template').content;
 
 /* page elements const */
 const editButton = document.querySelector('.profile__edit-button');
@@ -43,10 +41,8 @@ const popupEditForm = document.querySelector('.popup__container_edit');
 const popupAdd = document.querySelector('.popup_add');
 const popupAddForm = popupAdd.querySelector('.popup__container_add');
 const popupAllCloseButtons = document.querySelectorAll('.popup__close-button');
-const popupEnlarge = document.querySelector('.popup-enlarge');
-const popupImage = document.querySelector('.popup__image');
+
 const allPopup = document.querySelectorAll('.popup');
-const popupCaption = document.querySelector('.popup__caption');
 const popupFullName = document.querySelector('#full-name');
 const popupVocation = document.querySelector('#vocation');
 const popupName = document.querySelector('#name');
@@ -54,6 +50,8 @@ const popupUrl = document.querySelector('#url');
 
 /* config */
 const popupClassMarker = 'popup_opened';
+const cardTemplate = '.element__template'
+const elements = document.querySelector('.elements'); // target for inserting new instances of Card
 const configValidation = {
   formSelector: '.popup__container',
   inputSelector: '.popup__input',
@@ -64,16 +62,6 @@ const configValidation = {
 };
 
 /* handlers etc */
-function deleteElementButtonHandler(evt) {
-  const elementToDelete = evt.target.closest('.element');
-  const elementImage = elementToDelete.querySelector('.element__image');
-  const elementLike = elementToDelete.querySelector('.element__like');
-  elementImage.removeEventListener('click', elementImageHandler);
-  elementLike.removeEventListener('click', likeButtonHandler);
-  evt.target.removeEventListener('click', deleteElementButtonHandler);
-  elementToDelete.remove();
-}
-
 function clearValidationErrors(formElement) {
   const allFormInputs = Array.from(formElement.querySelectorAll(configValidation.inputSelector));
   allFormInputs.map(input => hideInputError(formElement, input, configValidation.inputErrorClass, configValidation.errorClass));
@@ -85,16 +73,13 @@ function removeEventListenerFromPopup(evt) {
 
 function closeAnyPopup(popupClassMarker, currentForm) {
   const popupToClose = document.querySelector(`.${popupClassMarker}`);
-  if (currentForm != null) { clearValidationErrors(currentForm); }
   removeEventListenerFromPopup();
-  if (popupToClose != null) { popupToClose.classList.remove(popupClassMarker); }
+ if (popupToClose != null)  popupToClose.classList.remove(popupClassMarker);
 }
 
 function closePopupAtEscape(evt) {
   if (evt.key === 'Escape') {
-    const currentPopup = document.querySelector(`.${popupClassMarker}`);
-    const currentForm = currentPopup.querySelector(`${configValidation.formSelector}`)
-    closeAnyPopup(popupClassMarker, currentForm);
+    closeAnyPopup(popupClassMarker);
   }
 }
 
@@ -102,15 +87,7 @@ function openPopupAddEventListener(popup, popupClassMarker) {
   document.addEventListener('keyup', closePopupAtEscape);
   popup.classList.add(popupClassMarker);
 }
-function likeButtonHandler(evt) {
-  evt.target.classList.toggle('element__like_active');
-}
 
-function elementImageHandler(evt) {
-  popupImage.src = evt.target.src;
-  popupCaption.textContent = evt.target.alt;
-  openPopupAddEventListener(popupEnlarge, popupClassMarker);
-}
 function closePopupAtOverlayClick(evt) {
   if (this === evt.target) {
     const currentForm = evt.target.querySelector(configValidation.formSelector);
@@ -118,32 +95,22 @@ function closePopupAtOverlayClick(evt) {
   }
 }
 
-function prepareNewElement(name, url) {
-  const element = elementTemplate.cloneNode(true);
-  const elementImage = element.querySelector('.element__image');
-  const deleteButton = element.querySelector('.element__delete-button');
-  const elementLike = element.querySelector('.element__like');
-  const elementTitle = element.querySelector('.element__title');
-  elementTitle.textContent = name;
-  elementImage.src = url;
-  elementImage.alt = elementTitle.textContent;
-  deleteButton.addEventListener('click', deleteElementButtonHandler);
-  elementLike.addEventListener('click', likeButtonHandler);
-  elementImage.addEventListener('click', elementImageHandler);
-  return element;
+function appendElement(name, url, targetElement) {
+  const card = new Card(name,url,'.element__template')
+  const cardElement = card.generateCard();
+  targetElement.prepend(cardElement);
 }
 
-function appendInitialElement(name, url, targetElement) {
-  targetElement.prepend(prepareNewElement(name, url));
-}
-
-function appendAllInitialElements() {
-  initialElements.forEach((item, index) => appendInitialElement(initialElements[index].title, initialElements[index].imageLink, elements));
-}
-
-appendAllInitialElements();
-
-/* EDIT FORM */
+function appendInitialElements () {
+  initialElements.forEach((item) => {
+    // Creating instance of Card class
+    const card = new Card(item.title, item.url,'.element__template');
+    // Filling instance with data and adding to DOM
+    const cardElement = card.generateCard();
+    elements.prepend(cardElement);
+  });
+  }
+  appendInitialElements();
 
 function formEditHandler(evt) {
   evt.preventDefault();
@@ -153,14 +120,14 @@ function formEditHandler(evt) {
 }
 
 function editButtonHandler() {
+  clearValidationErrors(popupEditForm);
   popupFullName.value = profileFullName.textContent;
   popupVocation.value = profileVocation.textContent;
   openPopupAddEventListener(popupEdit, popupClassMarker);
 }
 
 function closeButtonHandler(evt) {
-  const currentForm = document.querySelector(`.${event.target.parentElement.classList[1]}`);
-  closeAnyPopup(popupClassMarker, currentForm);
+  closeAnyPopup(popupClassMarker);
 }
 
 function clearPopupValues() {
@@ -169,17 +136,14 @@ function clearPopupValues() {
 }
 
 function addButtonHandler() {
+  clearValidationErrors(popupAddForm);
   clearPopupValues();
   openPopupAddEventListener(popupAdd, popupClassMarker);
 }
 
-function appendNewElement(popupName, popupUrl, targetElement) {
-  targetElement.prepend(prepareNewElement(popupName, popupUrl));
-}
-
 function formAddHandler(evt) {
   evt.preventDefault();
-  appendNewElement(popupName.value, popupUrl.value, elements);
+  appendElement(popupName.value, popupUrl.value, elements);
   closeAnyPopup(popupClassMarker);
 }
 
@@ -190,4 +154,8 @@ popupAddForm.addEventListener('submit', formAddHandler);
 popupEditForm.addEventListener('submit', formEditHandler);
 allPopup.forEach((item) => item.addEventListener('mousedown', closePopupAtOverlayClick));
 
->>>>>>> final-fixes
+//пока вручную, для каждой формы согласно заданию
+const editForm = new FormValidator(configValidation,".popup__container_edit");
+const addForm = new FormValidator(configValidation,".popup__container_add");
+editForm.enableValidation();
+addForm.enableValidation();
